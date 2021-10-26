430c7089db1718b398e956a047924e8083a91d89
riyadh9292 <mrriyadh66@gmail.com>
2021-10-27 01:10:51 +0600

    setting port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const cors = require("cors");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
